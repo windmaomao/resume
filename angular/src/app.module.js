d7135203346ea65ab321b85259c11fdfbafb6fc5
Fang Jin <windmaomao@gmail.com>
2016-10-07 13:48:33 -0400

    add angular to bower component of assets

---
+++ angular/src/app.module.js
@@ -0,0 +1,33 @@
+/**
+ * App entry module
+ *
+ * @date 10/7/16
+ * @author Fang Jin <windmaomao@gmail.com>
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angulars
+        // '$http', '$q',
+        // 'ngMessages'
+
+        // Vendors
+        // 'ui.router',             // router
+        // 'restangular',           // api
+        // 'localytics.directives'  // chosen
+        // 'ngDialog'               // dialog
+        // 'fillHeight'             // fill height
+        // 'ui.bootstrap',          // bootstrap
+        // 'ui.slimscroll',         // scroll
+
+        // Features
+        // 'app.lodash',               // -> _
+        // 'app.api',                  // -> central
+        'app.route',
+        // 'app.layout',
+        // 'app.aof',
+    ]);
+
+})();
