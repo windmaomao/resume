b0a92c5dc3bbd98b1d2bac5be0415c0bc0e9fa50
Fang Jin <windmaomao@gmail.com>
2016-10-09 10:17:58 -0400

    add more layout components

--- angular2/app/app.component.ts
+++ angular2/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<cv-navbar></cv-navbar>',
+  template: `
+    <cv-navbar></cv-navbar>
+    <div class="container" id="wrap">
+      <cv-version></cv-version>
+      <cv-header></cv-header>
+      <cv-footer></cv-footer>
+    </div>
+  `
 })
 export class AppComponent { }
 
@@ -11,3 +18,21 @@ export class AppComponent { }
   templateUrl: 'templates/layout.navbar.html'
 })
 export class CVNavbarComponent { }
+
+@Component({
+  selector: 'cv-version',
+  templateUrl: 'templates/layout.version.html'
+})
+export class CVVersionComponent { }
+
+@Component({
+  selector: 'cv-header',
+  templateUrl: 'templates/layout.header.html'
+})
+export class CVHeaderComponent { }
+
+@Component({
+  selector: 'cv-footer',
+  templateUrl: 'templates/layout.footer.html'
+})
+export class CVFooterComponent { }
--- angular2/app/app.module.ts
+++ angular2/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent, CVNavbarComponent }  from './app.component';
+import {
+  AppComponent,
+  CVNavbarComponent, CVVersionComponent,
+  CVHeaderComponent, CVFooterComponent
+}  from './app.component';
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, CVNavbarComponent ],
+  declarations: [
+    AppComponent,
+    CVNavbarComponent, CVVersionComponent,
+    CVHeaderComponent, CVFooterComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
