448368cdc8a24fb232ccad3d46a37597b591766a
Fang Jin <windmaomao@gmail.com>
2016-10-11 16:54:50 -0400

    add experience section

--- angular2/app/profile.service.ts
+++ angular2/app/profile.service.ts
@@ -177,9 +177,195 @@ export class ProfileService {
               'Page action: <em>Excel</em> like validation and action rules based on user input',
               'MVC Model: Extremely fat model design in <em>MVC</em> approach handling business logic'
             ]
+          },
+          {
+            description: "Provided <em>NodeJS</em> backend / <em>AngularJS</em> UI for report delivery and SLA breach notification system for 1000 daily reports from US West and East region. I served as the lead architect and developer for data API, time critical SLA breach as well as batch email summary generation and delivery. <small>since Mar 2016 </small>",
+            tasks: [
+              'Prototype of admin UI with <em>webpack</em>&nbsp; ',
+              'Responsive layout with material design ',
+              'Establish entity model with configuration file ',
+              'Grid style with grouping for large data exploration ',
+              'Design API spec for <em>MVC</em> rest service with <em>Mongo</em> ',
+              'Blueprint <em>MVC</em> routes with custom Kingslanding <em>NPM</em> ',
+              'Design SLA notification process with <em>Cognos</em> ',
+              'Multiple mixed level of serial and parallel process ',
+              'Unified logging for all backend tasks ',
+              'Streamlined data migration with excel in production environment ',
+              'Efficient shared data pool for fetching data ',
+              'Generation of report status email with summary table ',
+              'Scriptable schedules with priority and concurrency ',
+              'Plugin system for extendibility of backend system ',
+            ]
+          },
+          {
+            description: "Served as a lead front-end architect of <em>AngularJS </em>/ <em>NodeJS</em> and <em>UI/UX</em> for an enterprise statement 4-eyes-check solution for 25,000 customers worldwide. I also <strong>served as the sprint master for 8 people team (FA, QA, Dev and Owner etc.) in daily basis. I drafted the migration plan and collected the preliminary analysis</strong> on-boarding 25,000 users to the company banking entitlement system. <small>Since Sep 2015</small> ",
+            tasks: [
+              'Work with <em>Windows</em> environment ',
+              'Design page layout with <em>sketch</em> ',
+              'Design front-end framework with custom <em>grunt</em> build ',
+              'Develop recipient email list directive ',
+              'Unit test controller, directive and services ',
+              'Generate code coverage 80% above ',
+              'Perform parallel tasks on page load ',
+              'Design multiple entity selector workflow ',
+              'Add CSRF token protection for security ',
+              'Add command line tool via flatiron ',
+              'Work with <em>mongoDB</em> (monk) for bookkeeping ',
+              'Work with <em>Oracle</em> database query and dump ',
+              'Use request to issue service call to on-board user ',
+            ]
           }
         ]
-      }
+      },
+      {
+        title: 'Contracting',
+        subtitle: 'Drupal, Laravel, and AngularJS Developer',
+        period: 'Jun 2010 - Oct 2015',
+        description: "Since 2010, I started to build professional website as a freelancer using <em>PHP </em>and <em>Drupal.</em> Between 2013 and 2015, I transitioned from a backend developer to front-end UI/UX developer. More than 20 applications have been developed and deployed commercially, mainly using <em>Drupal,</em> <em>Laravel</em> and <em>AngularJS</em>."
+      },
+      {
+        title: 'Medicision, Raleigh NC',
+        subtitle: 'AngularJS developer and UI/UX designer',
+        period: 'July - Sep 2015',
+        description: "Provided UI mockup and <em>AngularJS</em> architect for Automated service center tool that will allow sales to setup customer environments based on their needs.&nbsp;My job is to <strong>work with an account manager to find out the business need</strong>. Both UI and <em>AngularJS</em> architect are drafted for both desktop and mobile audience."
+      },
+      {
+        title: 'NewStone Realty, Raleigh NC',
+        subtitle: 'Lead AngularJS/NodeJS architect',
+        period: 'Jan 2015 - Jun 2015',
+        description: 'Provided <em>AngularJS</em> architect and development of current realty website, especially in providing new front-end and backend framework to support the existing unstructured HTML content.&nbsp; Visit live site <a href="newstonerealty.com" target="_blank"><i class="icon ion-android-home"></i> here </a>.'
+      },
+      {
+        title: 'TopFitPro, Raleigh NC',
+        subtitle: 'Lead AngularJS/NodeJS architect',
+        period: 'Jan 2015 - Dec 2015',
+        description: 'Provided <em>AngularJS</em> (front) and <em>NodeJS</em> (back) architect and development for recipe ingredients guide application where users are guided to select healthy food based on carbs, proteins, fats and other personal preference. Internal state machine is built so that user will be suggested with choices based on six guidance step by step. At each step, user might be stopped to ask to substitute with healthy food or adjust food quantities based on personal preference. '
+      },
+      {
+        title: 'PeopleDesigns, Durham NC',
+        subtitle: 'Lead Laravel and AngularJS developer',
+        period: 'May 2014 - JUN 2015',
+        projects: [
+          {
+            description: "Provided <em>AngularJS</em> (front) and <em>NodeJS</em> (back) architect and development for southern regional center of excellence in nutirion education and obesity prevention. An introduction website presents information on programs, constituents and question and answer forum on the front while in the back manager can update the information daily through the same unified UI. Both customer and manager can interact the site in a responsive way when manager have more permissions in updating their permitted content and managing the workflow. ",
+            tasks: [
+              'Theme Design: Prototyped with <em>Bootstrap</em> metronic responsive template ',
+              'Fonte-end Prototype: AngularJS and <em>Bootstrap</em> custom modular development ',
+              'Rest API: NodeJS setup for <em>Rest</em> API with support of JSON file for fast prototype ',
+              'Token Based Authentication: NodeJS and <em>AngularJS</em> JWS token based authentication ',
+              'Google Calendar Integration: Parse <em>Google</em> public calendar via feed and iCal ',
+              'Questions and Comments: build-in question and commenting system ',
+              'Unified UI: Same UI for backend and frontend two possible ways to manage contents ',
+              'Map Integration: integrate <em>leaflet</em> map to display networks for selected US states ',
+              'WYSIWYG Editor: Rich text editor with file attachment integration ',
+              'File Management: Manage upload directory and files ',
+              '<strong><em>WYSIWYG</em></strong> Extension: Insert local file or picture to <strong><em>WYSIWYG</em></strong><em> </em>editor ',
+            ]
+          },
+          {
+            description: "Provided Laravel 4 architect and development for new version of PrepareForYourCare, a hospital stage III counseling program to help people prepare the end of their life. The project creates flexible tree-kind chapters navigation with video/narration helper for elder visits. 200 doctors and 50,000 users around the globe have used the site. ",
+            tasks: [
+              'HIPPA Server: <em>HIPPA</em> server host and deployment plan protecting health care privacy ',
+              'Authentication: Two factor <em>SMS</em> enabled authentication for better security protection ',
+              'Password Recovery: Recovery based on either username or 3 out of 6 question answers ',
+              '508 section: Disability compliance with video, and narration support with high contrast&nbsp; ',
+              'Multilingual Support: Three language translations made for pages, snippets and graphics ',
+              'Responsive Design: <em>Bootstrap</em> is used for fast prototype for desktop, iPad and other devices ',
+              'Template Design: Allow manager to modify each page content stored in file storage ',
+              'File Management: administer language files through web interface with easy switch ',
+              'Browser Compatibility: Support <em>Chrome,</em> <em>Safari,</em> <em>Firefox</em> and <em>IE</em> 8, 9 and 10 ',
+            ]
+          }
+        ]
+      },
+      {
+        title: 'RxAnte, Durham NC',
+        subtitle: 'Lead Drupal developer',
+        period: 'Sep 2014 - Mar 2015',
+        projects: [
+          {
+            description: 'At 2014, I joined a second stage healthcare startup company working as a <em>Drupal </em>developer. Provided enterprise <em>PHP/Drupal</em> development, support and deployment for a hospital/insurance/doctor relationship web application which helps their patients to adhere with their existing medications therefore upgrade ranking star in the healthcare system. The project creates dashboard to present data collected from doctor office and insurance company, and try to predict the patient behaviors. Analyzed data for each account (multi-tenant) are sampled from 10-20 contracts with average 0.5 to 5 millions patients.',
+            tasks: [
+              'Environment: <em>Drupal</em> 7 deployed in <em>CentOS</em> with vagrant support',
+              'HIPPA Server: <em>HIPPA</em> for health care privacy with two factor VPN protections',
+              'Continuous Integration: Continuousintegrated dev serverwith auto <em>Git</em> pull',
+              'Service API: Use <em>Slim</em> framework to build rest service API that hosts all data',
+              'Theming: <em>Drupal</em> theme and renderable array are extensively used ',
+              ' <em>IE</em> 8 Compatibility: Responsive background, round corner, table-cell and <strong><em>CSS</em></strong> compression ',
+            ]
+          }
+        ]
+      },
+      {
+        title: 'DesignHammer, Durham NC',
+        subtitle: 'Lead Drupal Developer',
+        period: 'Nov 2013 - Apr 2014',
+        projects: [
+          {
+            description: 'Provided <em>Drupal</em> 7 architect and development for a hospital startup environmental excellence awards application for more accurately measure and track sustainability performance of hospitals by allowing members to assess and track sustainability accomplishments. 320 awards (by 2013) are given to health care facilities and business members that show achievement in varying levels of excellence in sustainability overt he course of the calendar year. The project evolves into discovery, phase I, phase II and deployment stages, and took 9 month and 2000 billable hours by a team with two developers (me as the lead) and one project manager. The site has been launched with stats of 800,000 responses, 200 hospitals and 25,000 peek daily page views. ',
+            tasks: [
+              'Drupal Migration: Custom module to handle large complicated dataset migration from legacy <em>Excel</em> sheets ',
+              'Organic Group: When application is created, 8000 questionnaires auto-populated inside ',
+              'Form API: Questions are presented using Form API with input type dynamically populated from database ',
+              'Dashboard Design: Display all application percentage completion status for each client ',
+              'Review Workflow: Reviewer dashboard to assign, track and manage review process ',
+              '<em>Cron</em> &amp; Batch: Batch job is heavily used for time-consuming functionalities, ex. Creation and reporting ',
+              'Mathematic Formula: Excel cell calculation is implemented between questions upon front-end ',
+              'Score Management: Points are assigned for each question, and totaled for each application ',
+              'Report &amp; Export: Mass data assignment with import and export capability ',
+              'Browser Compatibility: <em>IE</em> 7,8,9 38% user using IE 7 and 96% using Windows ',
+            ]
+          }
+        ]
+      },
+      {
+        title: 'AlphaMed Press, Durham NC',
+        subtitle: 'Lead Drupal Architect',
+        period: 'Sep 2013 - Feb 2014',
+        projects: [
+          {
+            description: 'Redesign in <em>Drupal</em> 7 architect and migration from legacy Joomla site of two flagship sites Stemcell and Oncologist integrated with iPhone/iPad app using “One Web” solution and pulling articles daily through Stanford Hirewire. The site requires 8 user roles, 22 content types, 26 custom modules and 160 admin views. They host 3000 articles, 48,000 registered users and 120,000 unique visits per month. ',
+            tasks: [
+              'Data Modeling: article, interactive Ads, image, video and poll etc. ',
+              'Data Migration: custom migration from Stanford <em>HighWire</em> ',
+              'User Interface: desktop / mobile theming and interaction ',
+              'Content Management: import/export and manage content categories ',
+            ]
+          }
+        ]
+      },
+      {
+        title: 'Duke University, Durham NC',
+        subtitle: 'Lead Drupal Developer',
+        period: 'Jan - Dec 2013',
+        projects: [
+          {
+            description: 'Meantime, I also developed couple of front facing websites for Duke medical center and Duke university using <em>Drupal.</em> Redesign in <em>Drupal</em> 7 architect of department landing page and new students advising network application. The project splits into discovery, development, theming and support stages, and requires 300 panel layouts, 12 content types, 10 custom modules and 60 custom views and took 8 months and 1000 billable hours to finish for single developer. After the launch the site it has been used by 1,400 undergrads, 640 faculties in 50 majors.',
+            tasks: [
+              'Data Modeling: page, panel and widgets ',
+              'Layout Design: create various combinations of page layouts ',
+              'User Interface: apply layout pattern to multi departments ',
+              'Directory Search: integrated with internal directory search ',
+            ]
+          }
+        ]
+      },
+      {
+        title: 'PlusDelta Technologies, Durham NC',
+        subtitle: 'Lead PHP/Drupal Architect',
+        period: 'Jun 2012 - Dec 2013',
+        projects: [
+          {
+            description: 'At 2012, I joined a startup healthcare company, worked as a <em>Drupal</em> developer building a startup flagship product that tracks medication inside hospitals like FedEx. I help build a medication tracking and workflow process improvement solution for hospital pharmacies that uses handheld smart mobile technology with an integrated web-enabled software that verifies, records and monitors every step in the process of medication preparation, dispensing, and delivery. The project mainly builds an API that can talk to mobile devices and takes 1year with 2000 billable hours to finish. It has been deployed and used by 4 hospitals.',
+            tasks: [
+              'Data Modeling: dispense, med, order, and locations',
+              'Tracking Workflow: design workflow states for medications',
+              'Mobile Integration: design <em>API</em> to connect with devices&nbsp; ',
+              'Tracking Wallboard: large screen monitor for hospital activity ',
+            ]
+          }
+        ]
+      },
     ]
   }
 
