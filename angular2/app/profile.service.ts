dbac82a65d7286bb48034829c6a0aa63b5cbc41f
Fang Jin <windmaomao@gmail.com>
2016-10-10 21:39:53 -0400

    support html tag in description

--- angular2/app/profile.service.ts
+++ angular2/app/profile.service.ts
@@ -24,7 +24,7 @@ export class ProfileService {
     this.email = 'windmaomao @ gmail';
     this.phone = '(949) 302-7928';
     this.location = 'Raleigh, North Carolina';
-    this.description = 'I specialize in developing websites in healthcare, banking, real estate and university sectors. Overall my projects have reached 11 million users in production. I enjoy drafting and architecting front facing websites that talk to large enterprise dataset, most of time involving UI/UX design tailored for your business workflow.';
+    this.description = 'I specialize in developing websites in healthcare, banking, real estate and university sectors. Overall my projects have reached 11 million users in production. I enjoy drafting and architecting front facing websites that talk to large enterprise dataset, most of time involving <em>UI/UX</em> design tailored for your business workflow.';
     this.keywords = ['AngularJS', 'Sass', 'Bootstrap', 'Gulp', 'Git'];
   }
 }
