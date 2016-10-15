f416ab3ada3afca4cbb33f1c39b3f9e15311a17f
Fang Jin <windmaomao@gmail.com>
2016-10-15 14:23:51 -0400

    add layout component

--- ng2/src/app/app.component.ts
+++ ng2/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <app-layout></app-layout>
+  `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
--- ng2/src/app/app.module.ts
+++ ng2/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }      from '@angular/platform-browser';
+import { NgModule }           from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HighlightDirective } from './shared/highlight.directive';
+import { AppComponent }       from './app.component';
+import { LayoutComponent }    from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
+    LayoutComponent,
+    // CVNavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    // FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
