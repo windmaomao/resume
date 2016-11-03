481b3a956f06a5812b4977cca47bbced267c5959
Fang Jin <windmaomao@gmail.com>
2016-11-03 17:31:36 -0400

    add more gulp

--- ts/gulpfile.js
+++ ts/gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require('gulp');
-var tslint = require('gulp-tslint');
-var ts = require('gulp-typescript');
+var gulp        = require('gulp'),
+    tslint      = require('gulp-tslint'),
+    ts          = require('gulp-typescript'),
+    karma       = require('gulp-karma'),
+    browserify  = require('browserify'),
+    transform   = require('vinyl-transform'),
+    uglify      = require('gulp-uglify'),
+    sourcemaps  = require('gulp-sourcemaps');
 
 var tsProject = ts.createProject({
   removeComments: true,
@@ -21,5 +26,26 @@ gulp.task('tsc', function() {
     .pipe(tsProject())
     .js.pipe(gulp.dest('./tmp'));
 });
+
+gulp.task('karma', function(cb) {
+  return gulp.src('./dist/test/**/**.test.js')
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('end', cb)
+    .on('error', function(err) { throw err; });
+});
+
+
+
+// gulp.task('bundle-js', function() {
+//   return gulp.src(['./tmp/main.js'])
+//     .pipe(browserify({ debug: true }))
+//     .pipe(sourcemaps.init({ loadMaps: true }))
+//     .pipe(uglify())
+//     .pipe(sourcemaps.write('./'))
+//     .pipe(gulp.dest('./dist/js/'));
+// });
 
 gulp.task('default', ['lint', 'tsc']);
