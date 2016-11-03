9cb8eb1b96fa9bea6250c92d6ff3afaf5a5bf8e1
Fang Jin <windmaomao@gmail.com>
2016-11-03 17:04:30 -0400

    use gulp for tsc task

--- ts/gulpfile.js
+++ ts/gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var tslint = require('gulp-tslint');
+var ts = require('gulp-typescript');
 
-gulp.task('default', function() {
-  console.log('Hello gulp!');
+var tsProject = ts.createProject({
+  removeComments: true,
+  noImplicitAny: true,
+  target: 'ES3',
+  module: 'commonjs',
+  declarationFiles: false
 });
 
 gulp.task('lint', function() {
@@ -10,3 +15,11 @@ gulp.task('lint', function() {
     .pipe(tslint({ formatter: "verbose" }))
     .pipe(tslint.report());
 });
+
+gulp.task('tsc', function() {
+  return gulp.src(['./src/**/**.ts'])
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('default', ['lint', 'tsc']);
